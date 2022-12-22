d28469f11c82f2291eb5227cd278a46024a5790a
Ubuntu <ubuntu@ip-172-31-90-248.ec2.internal>
2022-12-22 14:26:49 +0000

    update

--- scripts/php.sh
+++ scripts/php.sh
@@ -15,6 +15,10 @@ sudo apt update
 
 sudo apt install php8.2 -y
 
+# install php8.2 extensions and dependencies
+
+sudo apt install php8.2 libapache2-mod-php php8.2-dev php8.2-zip php8.2-curl php8.2-mbstring php8.2-mysql php8.2-gd php8.2-xml php8.2-fpm -y
+
 # print php version
 
 php -v
